f907a80c3955b1de62f964dd8ac3b62df1891e9d
Sai Chowdary <sai1259725@gmail.com>
2025-12-10 20:39:20 +0530

    Feature: Terraform main module

---
+++ main.tf
@@ -0,0 +1,76 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "south india"
+}
+
+module "vnet" {
+  source = "./Modules/Vnet-module"
+  rgname = azurerm_resource_group.rg.name
+  vnetname = "Sai-Vnet"
+  rglocation = azurerm_resource_group.rg.location
+}
+
+module "vnet2" {
+  source = "./Modules/Vnet-module"
+  rgname = azurerm_resource_group.rg.name
+  rglocation = azurerm_resource_group.rg.location
+  vnetname = "Ramya-Vnet"
+
+}
+
+
+module "vm" {
+  source = "./Modules/Vm-module"
+  rgname = azurerm_resource_group.rg.name
+  rglocation = azurerm_resource_group.rg.location
+  subnetid = module.vnet2.subnetid
+  vmsize = "Standard_D2as_v5"
+  vmname = "Dev-Vm"
+  vmusername = "Sai"
+  vmpassword = "Puligundla@2025"
+  
+}
+
+/*
+module "vm1" {
+  source = "./Modules/Vm-module"
+  rgname = azurerm_resource_group.rg.name
+  rglocation = azurerm_resource_group.rg.location
+  subnetid = module.vnet2.subnetid
+  vmsize = "Standard_D2as_v5"
+  vmname = "Prod-Vm"
+  vmusername = "Sai"
+  vmpassword = "Puligundla@2025"
+  
+}
+*/
+
+/*
+module "vm1" {
+  source = "./Modules/Vm-module"
+  rgname = azurerm_resource_group.rg.name
+  rglocation = azurerm_resource_group.rg.location
+  subnetid = module.vnet2.subnetid
+  vmsize = "Standard_D2as_v5"
+  vmname = "Linux-Vm"
+  vmusername = "Sai"
+  vmpassword = "Puligundla@2025"
+  
+}
+*/
